142df03f8d05de81d2e93a15881d2aa0895c3630
Danillo Isaque Brito Lima <danilloisaq@gmail.com>
2023-05-12 15:58:10 -0300

    cadastrar e listar da categoria

---
+++ mercadoApp/src/DTO/categoriaDTO.java
@@ -0,0 +1,32 @@
+
+
+package DTO;
+
+
+public class categoriaDTO {
+
+    private int idCategoria;
+    
+    private String nomeCategoria;
+   
+    public int getIdCategoria() {
+        return idCategoria;
+    }
+
+  
+    public void setIdCategoria(int idCategoria) {
+        this.idCategoria = idCategoria;
+    }
+
+   
+    public String getNomeCategoria() {
+        return nomeCategoria;
+    }
+
+    
+    public void setNomeCategoria(String nomeCategoria) {
+        this.nomeCategoria = nomeCategoria;
+    }
+    
+
+}
